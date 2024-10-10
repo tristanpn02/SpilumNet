84c640032e740cfa12bd6afe44c8702e773c3fd1
Tristan Andersen <tpan02@pm.me>
2024-10-10 01:48:54 +0000

    Copy Home page to About page

---
+++ src/Pages/About/index.tsx
@@ -0,0 +1,14 @@
+const About = () => {
+    return (
+        <div className="home">
+            <h1>Welcome to Spilum.Net</h1>
+            <h2>What is this website?</h2>
+
+            <p>This website doesn't have a particular purpose, other than being my personal programming sandbox.
+                However, you might find some useful resources here, so feel free to make use of them.
+            </p>
+        </div>
+    )
+}
+
+export default About;
