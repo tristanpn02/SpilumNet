7662a7b4ebf3e0cb585143f8d4c0344cba4cf1d8
Tristan Andersen <tpan02@pm.me>
2024-10-10 22:18:24 +0000

    Await Axios.get

--- src/api/jsonPlaceholder.tsx
+++ src/api/jsonPlaceholder.tsx
@@ -3,16 +3,16 @@ import axios from "axios";
 const API_URL = 'https://jsonplaceholder.typicode.com';
 
 export const fetchPosts = async () => {
-    const response = axios.get(`${API_URL}/posts`);
+    const response = await axios.get(`${API_URL}/posts`);
     return response.data;
 }
 
 export const fetchUsers = async () => {
-    const response = axios.get(`${API_URL}/users`);
+    const response = await axios.get(`${API_URL}/users`);
     return response.data;
 }
 
 export const fetchComments = async () => {
-    const response = axios.get(`${API_URL}/comments`);
+    const response = await axios.get(`${API_URL}/comments`);
     return response.data;
 }
