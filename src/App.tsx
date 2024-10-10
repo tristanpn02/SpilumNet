08753b6e77b661788eaa365babcbccb48dc2b6a4
Tristan Andersen <tpan02@pm.me>
2024-10-10 01:10:42 +0000

    Create Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import './App.css'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 
+import Home from './Pages/Home'
+
 function App() {
 
   return (
     <>
       <Header />
+      <Home />
       <Footer />
     </>
   )
