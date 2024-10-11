35396088b8c80ec265064f6302f1cf41c181858a
Tristan Andersen <tpan02@pm.me>
2024-10-11 22:41:26 +0000

    Move Link to cover post list item

--- src/Components/Posts/index.tsx
+++ src/Components/Posts/index.tsx
@@ -58,19 +58,20 @@ const Posts = () => {
             <h2>Posts</h2>
             <ul>
                 {posts.map(post => (
-                    <li key={post.id}>
-                        <Link to={`/post/${post.id}`}>
-                            <h3>{post.title}</h3>
-                        
-                        {users[post.userId] ? (
-                            <Link to={`/user/${post.userId}`}>
-                                By {users[post.userId].name}
-                            </Link>
-                        ) : (
-                            <p>Loading user...</p>
-                        )}
-                        </Link>
-                    </li>
+                    <Link to={`/post/${post.id}`}>
+                        <li key={post.id}>
+                            
+                                <h3>{post.title}</h3>
+                            
+                            {users[post.userId] ? (
+                                <Link to={`/user/${post.userId}`}>
+                                    By {users[post.userId].name}
+                                </Link>
+                            ) : (
+                                <p>Loading user...</p>
+                            )}
+                        </li>
+                    </Link>
                 ))}
             </ul>
         </div>
